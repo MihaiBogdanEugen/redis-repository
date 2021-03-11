d0e2f6a38f356854d9b43816d8eb0abb0b2f30e7
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2021-03-11 15:09:15 +0100

    hash repositories

--- src/main/java/com/github/mihaibogdaneugen/redisrepository/BinaryHashRedisRepository.java
+++ src/main/java/com/github/mihaibogdaneugen/redisrepository/BinaryHashRedisRepository.java
@@ -2,21 +2,28 @@ package com.github.mihaibogdaneugen.redisrepository;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Response;
 import redis.clients.jedis.util.SafeEncoder;
 
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public abstract class BinaryHashRedisRepository<T extends RedisEntity<byte[]>>
-        extends BaseRedisRepository<byte[]>
-        implements BinaryRepository<T> {
+public abstract class BinaryHashRedisRepository<T> extends BaseRedisRepository<T> {
 
-    public BinaryHashRedisRepository(final Jedis jedis, final RedisRepositoryConfiguration configuration) {
-        super(jedis, configuration);
+    private final String keyPrefix;
+    private final byte[] allKeysPattern;
+
+    public BinaryHashRedisRepository(final Jedis jedis, final String collectionKey) {
+        super(jedis);
+        keyPrefix = collectionKey + DEFAULT_KEY_SEPARATOR;
+        allKeysPattern = SafeEncoder.encode(collectionKey + DEFAULT_KEY_SEPARATOR + "*");
     }
 
-    public BinaryHashRedisRepository(final JedisPool jedisPool, final RedisRepositoryConfiguration configuration) {
-        super(jedisPool, configuration);
+    public BinaryHashRedisRepository(final JedisPool jedisPool, final String collectionKey) {
+        super(jedisPool);
+        keyPrefix = collectionKey + DEFAULT_KEY_SEPARATOR;
+        allKeysPattern = SafeEncoder.encode(collectionKey + DEFAULT_KEY_SEPARATOR + "*");
     }
 
     public abstract Map<byte[], byte[]> convertTo(final T entity);
@@ -24,18 +31,8 @@ public abstract class BinaryHashRedisRepository<T extends RedisEntity<byte[]>>
     public abstract T convertFrom(final Map<byte[], byte[]> entity);
 
     @Override
-    public final byte[] getKey(byte[] keySuffix) {
-        return SafeEncoder.encode(configuration.getKeyPrefix() + SafeEncoder.encode(keySuffix));
-    }
-
-    @Override
-    public final byte[] getAllKeysPattern() {
-        return SafeEncoder.encode(configuration.getKeyPrefix() + "*");
-    }
-
-    @Override
-    public final Optional<T> get(final byte[] id) {
-        throwIfNullOrEmpty(id, "id");
+    public final Optional<T> get(final String id) {
+        throwIfNullOrEmptyOrBlank(id, "id");
         final var key = getKey(id);
         final var entity = jedis.hgetAll(key);
         return isNullOrEmpty(entity)
@@ -44,16 +41,134 @@ public abstract class BinaryHashRedisRepository<T extends RedisEntity<byte[]>>
     }
 
     @Override
-    public final Boolean exists(final byte[] id) {
-        throwIfNullOrEmpty(id, "id");
+    public final List<T> get(final String... ids) {
+        throwIfNullOrEmpty(ids);
+        final var keys = getKeys(ids);
+        return getByKeys(keys);
+    }
+
+    @Override
+    public final List<T> getAll() {
+        final var keys = getAllKeys();
+        return getByKeys(keys);
+    }
+
+    @Override
+    public final Boolean exists(final String id) {
+        throwIfNullOrEmptyOrBlank(id, "id");
         final var key = getKey(id);
         return jedis.exists(key);
     }
 
     @Override
-    public final void set(final T entity) {
+    public final void set(final String id, final T entity) {
+        throwIfNullOrEmptyOrBlank(id, "id");
         throwIfNull(entity, "entity");
-        final var key = getKey(entity.getKey());
+        final var key = getKey(id);
         jedis.hset(key, convertTo(entity));
+    }
+
+    @Override
+    public final void setIfNotExist(final String id, final T entity) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        throwIfNull(entity, "entity");
+        final var key = getKey(id);
+        jedis.watch(key);
+        if (jedis.exists(key)) {
+            jedis.unwatch();
+            return;
+        }
+        try (final var transaction = jedis.multi()) {
+            transaction.hset(key, convertTo(entity));
+            transaction.exec();
+        }
+    }
+
+    @Override
+    public final Optional<Boolean> update(final String id, final Function<T, T> updater) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        throwIfNull(updater, "updater");
+        final var key = getKey(id);
+        jedis.watch(key);
+        final var value = jedis.hgetAll(key);
+        if (isNullOrEmpty(value)) {
+            jedis.unwatch();
+            return Optional.empty();
+        }
+        final var entity = convertFrom(value);
+        final var newEntity = updater.apply(entity);
+        final var newValue = convertTo(newEntity);
+        final List<Object> results;
+        try (final var transaction = jedis.multi()) {
+            transaction.hset(key, newValue);
+            results = transaction.exec();
+        }
+        return Optional.of(isNotNullNorEmpty(results));
+    }
+
+    @Override
+    public final void delete(final String id) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        final var key = getKey(id);
+        jedis.del(key);
+    }
+
+    @Override
+    public final void delete(final String... ids) {
+        throwIfNullOrEmpty(ids);
+        final var keys = getKeys(ids);
+        jedis.del(keys);
+    }
+
+    @Override
+    public final void deleteAll() {
+        final var keys = getAllKeys();
+        jedis.del(keys);
+    }
+
+    public final void setExpirationAfter(final String id, final long milliseconds) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        throwIfNegative(milliseconds, "milliseconds");
+        final var key = getKey(id);
+        jedis.pexpire(key, milliseconds);
+    }
+
+    public final void setExpirationAt(final String id, final long millisecondsTimestamp) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        throwIfNegative(millisecondsTimestamp, "millisecondsTimestamp");
+        final var key = getKey(id);
+        jedis.pexpireAt(key, millisecondsTimestamp);
+    }
+
+    public final Long getTimeToLiveLeft(final String id) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        final var key = getKey(id);
+        return jedis.pttl(key);
+    }
+
+    private List<T> getByKeys(final byte[]... keys) {
+        final var responses = new ArrayList<Response<Map<byte[], byte[]>>>();
+        try (final var pipeline = jedis.pipelined()) {
+            Arrays.stream(keys).forEach(key -> responses.add(pipeline.hgetAll(key)));
+            pipeline.sync();
+        }
+        return responses.stream()
+                .map(response -> convertFrom(response.get()))
+                .collect(Collectors.toList());
+    }
+
+    private byte[][] getKeys(final String... keySuffixes) {
+        return Arrays.stream(keySuffixes)
+                .filter(BaseRedisRepository::isNotNullNorEmptyNorBlank)
+                .map(this::getKey)
+                .toArray(byte[][]::new);
+    }
+
+    private byte[] getKey(final String keySuffix) {
+        return SafeEncoder.encode(keyPrefix + keySuffix);
+    }
+
+    private byte[][] getAllKeys() {
+        return jedis.keys(allKeysPattern).toArray(byte[][]::new);
     }
 }
--- src/main/java/com/github/mihaibogdaneugen/redisrepository/StringHashRedisRepository.java
+++ src/main/java/com/github/mihaibogdaneugen/redisrepository/StringHashRedisRepository.java
@@ -2,35 +2,34 @@ package com.github.mihaibogdaneugen.redisrepository;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Response;
 
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public abstract class StringHashRedisRepository<T extends RedisEntity<String>>
-        extends BaseRedisRepository<String>
-        implements StringRepository<T> {
+public abstract class StringHashRedisRepository<T> extends BaseRedisRepository<T> {
 
-    public StringHashRedisRepository(final Jedis jedis, final RedisRepositoryConfiguration configuration) {
-        super(jedis, configuration);
+    private final String keyPrefix;
+    private final String allKeysPattern;
+
+    public StringHashRedisRepository(final Jedis jedis, final String collectionKey) {
+        super(jedis);
+        throwIfNullOrEmptyOrBlank(collectionKey, "collectionKey");
+        keyPrefix = collectionKey + DEFAULT_KEY_SEPARATOR;
+        allKeysPattern = collectionKey + DEFAULT_KEY_SEPARATOR + "*";
     }
 
-    public StringHashRedisRepository(final JedisPool jedisPool, final RedisRepositoryConfiguration configuration) {
-        super(jedisPool, configuration);
+    public StringHashRedisRepository(final JedisPool jedisPool, final String collectionKey) {
+        super(jedisPool);
+        throwIfNullOrEmptyOrBlank(collectionKey, "collectionKey");
+        keyPrefix = collectionKey + DEFAULT_KEY_SEPARATOR;
+        allKeysPattern = collectionKey + DEFAULT_KEY_SEPARATOR + "*";
     }
 
     public abstract Map<String, String> convertTo(final T entity);
 
     public abstract T convertFrom(final Map<String, String> entity);
-
-    @Override
-    public final String getKey(final String keySuffix) {
-        return configuration.getKeyPrefix() + keySuffix;
-    }
-
-    @Override
-    public final String getAllKeysPattern() {
-        return configuration.getKeyPrefix() + "*";
-    }
 
     @Override
     public final Optional<T> get(final String id) {
@@ -43,6 +42,19 @@ public abstract class StringHashRedisRepository<T extends RedisEntity<String>>
     }
 
     @Override
+    public final List<T> get(final String... ids) {
+        throwIfNullOrEmpty(ids);
+        final var keys = getKeys(ids);
+        return getByKeys(keys);
+    }
+
+    @Override
+    public final List<T> getAll() {
+        final var keys = getAllKeys();
+        return getByKeys(keys);
+    }
+
+    @Override
     public final Boolean exists(final String id) {
         throwIfNullOrEmptyOrBlank(id, "id");
         final var key = getKey(id);
@@ -50,9 +62,114 @@ public abstract class StringHashRedisRepository<T extends RedisEntity<String>>
     }
 
     @Override
-    public final void set(final T entity) {
+    public final void set(final String id, final T entity) {
+        throwIfNullOrEmptyOrBlank(id, "id");
         throwIfNull(entity, "entity");
-        final var key = getKey(entity.getKey());
+        final var key = getKey(id);
         jedis.hset(key, convertTo(entity));
+    }
+
+    @Override
+    public final void setIfNotExist(final String id, final T entity) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        throwIfNull(entity, "entity");
+        final var key = getKey(id);
+        jedis.watch(key);
+        if (jedis.exists(key)) {
+            jedis.unwatch();
+            return;
+        }
+        try (final var transaction = jedis.multi()) {
+            transaction.hset(key, convertTo(entity));
+            transaction.exec();
+        }
+    }
+
+    @Override
+    public final Optional<Boolean> update(final String id, final Function<T, T> updater) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        throwIfNull(updater, "updater");
+        final var key = getKey(id);
+        jedis.watch(key);
+        final var value = jedis.hgetAll(key);
+        if (isNullOrEmpty(value)) {
+            jedis.unwatch();
+            return Optional.empty();
+        }
+        final var entity = convertFrom(value);
+        final var newEntity = updater.apply(entity);
+        final var newValue = convertTo(newEntity);
+        final List<Object> results;
+        try (final var transaction = jedis.multi()) {
+            transaction.hset(key, newValue);
+            results = transaction.exec();
+        }
+        return Optional.of(isNotNullNorEmpty(results));
+    }
+
+    @Override
+    public final void delete(final String id) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        final var key = getKey(id);
+        jedis.del(key);
+    }
+
+    @Override
+    public final void delete(final String... ids) {
+        throwIfNullOrEmpty(ids);
+        final var keys = getKeys(ids);
+        jedis.del(keys);
+    }
+
+    @Override
+    public final void deleteAll() {
+        final var keys = getAllKeys();
+        jedis.del(keys);
+    }
+
+    public final void setExpirationAfter(final String id, final long milliseconds) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        throwIfNegative(milliseconds, "milliseconds");
+        final var key = getKey(id);
+        jedis.pexpire(key, milliseconds);
+    }
+
+    public final void setExpirationAt(final String id, final long millisecondsTimestamp) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        throwIfNegative(millisecondsTimestamp, "millisecondsTimestamp");
+        final var key = getKey(id);
+        jedis.pexpireAt(key, millisecondsTimestamp);
+    }
+
+    public final Long getTimeToLiveLeft(final String id) {
+        throwIfNullOrEmptyOrBlank(id, "id");
+        final var key = getKey(id);
+        return jedis.pttl(key);
+    }
+
+    private List<T> getByKeys(final String... keys) {
+        final var responses = new ArrayList<Response<Map<String, String>>>();
+        try (final var pipeline = jedis.pipelined()) {
+            Arrays.stream(keys).forEach(key -> responses.add(pipeline.hgetAll(key)));
+            pipeline.sync();
+        }
+        return responses.stream()
+                .map(response -> convertFrom(response.get()))
+                .collect(Collectors.toList());
+    }
+
+    private String[] getKeys(final String... keySuffixes) {
+        return Arrays.stream(keySuffixes)
+                .filter(BaseRedisRepository::isNotNullNorEmptyNorBlank)
+                .map(this::getKey)
+                .toArray(String[]::new);
+    }
+
+    private String getKey(final String keySuffix) {
+        return keyPrefix + keySuffix;
+    }
+
+    private String[] getAllKeys() {
+        return jedis.keys(allKeysPattern).toArray(String[]::new);
     }
 }
