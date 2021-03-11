e4212a823c0abe4c0ed49708bf630b956035e11f
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2021-03-11 15:08:08 +0100

    simplify configuration, refactor base class

--- src/main/java/com/github/mihaibogdaneugen/redisrepository/BaseRedisRepository.java
+++ src/main/java/com/github/mihaibogdaneugen/redisrepository/BaseRedisRepository.java
@@ -5,38 +5,25 @@ import redis.clients.jedis.JedisPool;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-abstract class BaseRedisRepository<KeyType> implements AutoCloseable {
+abstract class BaseRedisRepository<T> implements RedisRepository<T>, AutoCloseable {
+
+    protected static final String DEFAULT_KEY_SEPARATOR = ":";
 
     protected final Jedis jedis;
-    protected final RedisRepositoryConfiguration configuration;
 
-    public BaseRedisRepository(final Jedis jedis, final RedisRepositoryConfiguration configuration) {
+    public BaseRedisRepository(final Jedis jedis) {
         throwIfNull(jedis, "jedis");
-        throwIfNull(configuration, "configuration");
         this.jedis = jedis;
-        this.configuration = configuration;
     }
 
-    public BaseRedisRepository(final JedisPool jedisPool, final RedisRepositoryConfiguration configuration) {
+    public BaseRedisRepository(final JedisPool jedisPool) {
         throwIfNull(jedisPool, "jedisPool");
-        throwIfNull(configuration, "configuration");
         this.jedis = jedisPool.getResource();
-        this.configuration = configuration;
-    }
-
-    protected KeyType getKey(final KeyType keySuffix) {
-        throw new UnsupportedOperationException();
-    }
-
-    protected KeyType getAllKeysPattern() {
-        throw new UnsupportedOperationException();
     }
 
     @Override
-    public void close() {
+    public final void close() {
         try {
             jedis.close();
         } catch (final Exception exception) {
@@ -50,9 +37,9 @@ abstract class BaseRedisRepository<KeyType> implements AutoCloseable {
         }
     }
 
-    protected static void throwIfNullOrEmpty(final byte[] bytes, final String valueName) {
-        if (isNullOrEmpty(bytes)) {
-            throw new IllegalArgumentException(valueName + " cannot be null, nor empty!");
+    protected static void throwIfNullOrEmpty(final String[] strings) {
+        if (isNullOrEmpty(strings)) {
+            throw new IllegalArgumentException("ids cannot be null, nor empty!");
         }
     }
 
@@ -62,8 +49,22 @@ abstract class BaseRedisRepository<KeyType> implements AutoCloseable {
         }
     }
 
+    protected static void throwIfNegative(final long value, final String valueName) {
+        if (value < 0) {
+            throw new IllegalArgumentException(valueName + " cannot have a negative value!");
+        }
+    }
+
     protected static boolean isNullOrEmpty(final byte[] bytes) {
         return bytes == null || bytes.length == 0;
+    }
+
+    protected static boolean isNullOrEmpty(final String[] strings) {
+        return strings == null || strings.length == 0;
+    }
+
+    protected static <T> boolean isNotNullNorEmpty(final List<T> list) {
+        return list != null && !list.isEmpty();
     }
 
     protected static <T> boolean isNullOrEmpty(final Map<T, T> map) {
@@ -74,9 +75,11 @@ abstract class BaseRedisRepository<KeyType> implements AutoCloseable {
         return text == null || text.isEmpty() || text.isBlank();
     }
 
-    protected static <T> List<List<T>> getBatches(final List<T> collection, final int batchSize) {
-        return IntStream.iterate(0, i -> i < collection.size(), i -> i + batchSize)
-                .mapToObj(i -> collection.subList(i, Math.min(i + batchSize, collection.size())))
-                .collect(Collectors.toList());
+    protected static boolean isNotNullNorEmptyNorBlank(final String text) {
+        return !isNullOrEmptyOrBlank(text);
+    }
+
+    protected static boolean isNotNullNorEmpty(final byte[] bytes) {
+        return !isNullOrEmpty(bytes);
     }
 }
