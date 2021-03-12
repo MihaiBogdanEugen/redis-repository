fb3d06a80b035d0640161aa0bc701048e49c881b
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2021-03-12 10:20:59 +0100

    test all delete methods

--- src/test/java/com/github/mihaibogdaneugen/redisrepository/BinaryRedisRepositoryTests.java
+++ src/test/java/com/github/mihaibogdaneugen/redisrepository/BinaryRedisRepositoryTests.java
@@ -13,10 +13,12 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.util.SafeEncoder;
 
 import java.io.UncheckedIOException;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static com.github.mihaibogdaneugen.redisrepository.BaseRedisRepository.isNullOrEmpty;
 import static org.junit.jupiter.api.Assertions.*;
 
 final class BinaryRedisRepositoryTests extends RedisTestContainer {
@@ -331,8 +333,142 @@ final class BinaryRedisRepositoryTests extends RedisTestContainer {
         assertFalse(repository.exists(expectedPerson2.getId()));
     }
 
+    @Test
+    void testDeleteInvalidArgument() {
+        final var nullIdError = assertThrows(IllegalArgumentException.class, () ->
+                repository.delete((String)null));
+        assertEquals("id cannot be null, nor empty!", nullIdError.getMessage());
+
+        final var emptyIdError = assertThrows(IllegalArgumentException.class, () ->
+                repository.delete(""));
+        assertEquals("id cannot be null, nor empty!", emptyIdError.getMessage());
+    }
+
+    @Test
+    void testDeleteNonExistentEntity() {
+        final var expectedPerson1 = Person.random();
+        insert(expectedPerson1);
+        final var expectedPerson2 = Person.random();
+        repository.delete(expectedPerson2.getId());
+        final var actualPerson1 = get(expectedPerson1.getId());
+        assertTrue(actualPerson1.isPresent());
+        assertEquals(expectedPerson1, actualPerson1.get());
+        final var actualPerson2 = get(expectedPerson2.getId());
+        assertTrue(actualPerson2.isEmpty());
+    }
+
+    @Test
+    void testDelete() {
+        final var expectedPerson1 = Person.random();
+        insert(expectedPerson1);
+        repository.delete(expectedPerson1.getId());
+        final var actualPerson1 = get(expectedPerson1.getId());
+        assertTrue(actualPerson1.isEmpty());
+    }
+
+    @Test
+    void testDeleteMultipleInvalidArgument() {
+        final var nullIdError = assertThrows(IllegalArgumentException.class, () ->
+                repository.delete((String[])null));
+        assertEquals("ids cannot be null, nor empty!", nullIdError.getMessage());
+
+        @SuppressWarnings("RedundantArrayCreation")
+        final var emptyIdError = assertThrows(IllegalArgumentException.class, () ->
+                repository.delete(new String[0]));
+        assertEquals("ids cannot be null, nor empty!", emptyIdError.getMessage());
+    }
+
+    @Test
+    void testDeleteMultipleExistingAndNonExistingEntities() {
+        final var expectedPerson1 = Person.random();
+        insert(expectedPerson1);
+        final var expectedPerson2 = Person.random();
+        insert(expectedPerson2);
+        final var expectedPerson3 = Person.random();
+        insert(expectedPerson3);
+        repository.delete(expectedPerson1.getId(), randomString(), expectedPerson3.getId());
+        final var actualPerson1 = get(expectedPerson1.getId());
+        final var actualPerson2 = get(expectedPerson2.getId());
+        final var actualPerson3 = get(expectedPerson3.getId());
+        assertTrue(actualPerson1.isEmpty());
+        assertTrue(actualPerson2.isPresent());
+        assertEquals(expectedPerson2, actualPerson2.get());
+        assertTrue(actualPerson3.isEmpty());
+    }
+
+    @Test
+    void testDeleteMultipleOnlySpecifiedEntities() {
+        final var expectedPerson1 = Person.random();
+        insert(expectedPerson1);
+        final var expectedPerson2 = Person.random();
+        insert(expectedPerson2);
+        final var expectedPerson3 = Person.random();
+        insert(expectedPerson3);
+        final var expectedPerson4 = Person.random();
+        insert(expectedPerson4);
+        final var expectedPerson5 = Person.random();
+        insert(expectedPerson5);
+        repository.delete(expectedPerson1.getId(), expectedPerson2.getId(), expectedPerson3.getId());
+        final var actualPerson1 = get(expectedPerson1.getId());
+        final var actualPerson2 = get(expectedPerson2.getId());
+        final var actualPerson3 = get(expectedPerson3.getId());
+        final var actualPerson4 = get(expectedPerson4.getId());
+        final var actualPerson5 = get(expectedPerson5.getId());
+        assertTrue(actualPerson1.isEmpty());
+        assertTrue(actualPerson2.isEmpty());
+        assertTrue(actualPerson3.isEmpty());
+        assertTrue(actualPerson4.isPresent());
+        assertEquals(expectedPerson4, actualPerson4.get());
+        assertTrue(actualPerson5.isPresent());
+        assertEquals(expectedPerson5, actualPerson5.get());
+    }
+
+    @Test
+    void testDeleteMultiple() {
+        final var expectedPeopleMap = IntStream.range(0, 50)
+                .mapToObj(i -> Person.random())
+                .collect(Collectors.toMap(Person::getId, person -> person));
+        expectedPeopleMap.values().forEach(this::insert);
+        repository.delete(expectedPeopleMap.keySet().toArray(String[]::new));
+        expectedPeopleMap.keySet().forEach(key -> {
+            final var result = get(key);
+            assertTrue(result.isEmpty());
+        });
+    }
+
+    @Test
+    void testDeleteAll() {
+        final var expectedPeopleMap = IntStream.range(0, 50)
+                .mapToObj(i -> Person.random())
+                .collect(Collectors.toMap(Person::getId, person -> person));
+        expectedPeopleMap.values().forEach(this::insert);
+        repository.deleteAll();
+        expectedPeopleMap.keySet().forEach(key -> {
+            final var result = get(key);
+            assertTrue(result.isEmpty());
+        });
+    }
+
+    @Test
+    void testDeleteAll1000() {
+        final var expectedPeopleMap = IntStream.range(0, 1000)
+                .mapToObj(i -> Person.random())
+                .collect(Collectors.toMap(Person::getId, person -> person));
+        expectedPeopleMap.values().forEach(this::insert);
+        repository.deleteAll();
+        expectedPeopleMap.keySet().forEach(key -> {
+            final var result = get(key);
+            assertTrue(result.isEmpty());
+        });
+    }
+
     private void insert(final Person person) {
         jedis.set(SafeEncoder.encode("people:" + person.getId()), repository.convertTo(person));
+    }
+
+    private Optional<Person> get(final String id) {
+        final var entity = jedis.get(SafeEncoder.encode("people:" + id));
+        return isNullOrEmpty(entity) ? Optional.empty() : Optional.of(repository.convertFrom(entity));
     }
 
     private String randomString() {
