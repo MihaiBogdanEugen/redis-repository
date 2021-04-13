00e86d5717442304fa2599c709a824b4cb2aae1b
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2021-04-13 23:22:27 +0200

    remove flaky tests

--- src/test/java/com/github/mihaibogdaneugen/redisrepository/RedisTestContainer.java
+++ src/test/java/com/github/mihaibogdaneugen/redisrepository/RedisTestContainer.java
@@ -1,9 +1,13 @@
 package com.github.mihaibogdaneugen.redisrepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
 abstract class RedisTestContainer {
+
+    static final Logger logger = LoggerFactory.getLogger(RedisTestContainer.class);
 
     static final int REDIS_PORT = 6379;
 
@@ -13,5 +17,14 @@ abstract class RedisTestContainer {
         REDIS_CONTAINER = new GenericContainer<>(DockerImageName.parse("redis:6"))
                 .withExposedPorts(REDIS_PORT);
         REDIS_CONTAINER.start();
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    static void safeThreadSleep(final long millis) {
+        try {
+             Thread.sleep(millis);
+        } catch (final InterruptedException e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 }
